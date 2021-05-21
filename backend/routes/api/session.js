0b72e669532ecce8607e0f3c573be10503f6905b
AlexBetita <alexbheb25@gmail.com>
2021-05-21 08:52:25 -0700

    navigation

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -59,4 +59,13 @@ router.get(
     }
 );
 
+// Log out
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;
