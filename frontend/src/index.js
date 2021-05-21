af2b873687e6e8add0fc17e32109827d7a07d660
AlexBetita <alexbheb25@gmail.com>
2021-05-21 08:35:18 -0700

    front end loginform

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,7 +10,7 @@ import App from './App';
 
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-import { login } from './store/session';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV !== 'production') {
 
   window.store = store;
   window.csrfFetch = csrfFetch;
-  window.login = login;
+  window.sessionActions = sessionActions;
 }
 
 function Root() {
