adfeee25d3c487cfa06c95ea852188bb90087bde
AlexBetita <alexbheb25@gmail.com>
2021-05-25 16:05:30 -0700

    worked on product hunt home page

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -20,21 +20,63 @@ function Navigation({ isLoaded }){
   } else {
     sessionLinks = (
       <>
-        <LoginFormModal />
+        <div className='div__navigation__styles__login'>
+          <LoginFormModal />
+          <SignupFormModal />
         {/* <NavLink to="/signup">Sign Up</NavLink> */}
-        <SignupFormModal />
+        </div>
       </>
     );
   }
 
   return (
-    <div className='div__navigation'>
-      <ul>
-        <li>
-          <NavLink exact to="/">Home</NavLink>
-          {isLoaded && sessionLinks}
-        </li>
-      </ul>
+    <div className='div__navigation__styles'>
+      <div className='div__navigation'>
+
+       <NavLink exact to="/">
+        <img className='img__producthunt__icon'
+            src='https://cdn2.iconfinder.com/data/icons/social-icons-33/128/Product_Hunt-512.png'>
+          </img>
+       </NavLink>
+
+        <div className='div__navigation__search__style'>
+          <form className='form__navigation__search'>
+            <label className='form__label__navigation__search'>
+              <img class='img__navigation__search'
+                src='https://cdn1.iconfinder.com/data/icons/hawcons/32/698956-icon-111-search-512.png'>
+              </img>
+              <input className='form__input__navigation__search' placeholder='Search Product Hunt'>
+              </input>
+              <button type='reset' hidden='true'>
+              </button>
+            </label>
+          </form>
+        </div>
+
+        <div className='div__navigation__styles__links'>
+
+          <NavLink exact to="/discussions" style={{ textDecoration: 'none', color: '#999A9C' }}>
+            <div className='div__navigation__discussions'>
+                Discussions
+              </div>
+          </NavLink>
+
+          <div className='div__navigation__deals'>
+            <NavLink exact to="/deals" style={{ textDecoration: 'none', color: '#999A9C' }}>Deals</NavLink>
+            </div>
+
+            <div className='div__navigation__jobs'>
+              <NavLink exact to="/jobs" style={{ textDecoration: 'none', color: '#999A9C' }}>Jobs</NavLink>
+            </div>
+            <div className='div__navigation__ship'>
+              <NavLink exact to="/ship" style={{ textDecoration: 'none', color: '#999A9C' }}>Ship</NavLink>
+            </div>
+            <label className='label__navigation__dropdown'>
+              ...
+            </label>
+        </div>
+        {isLoaded && sessionLinks}
+      </div>
     </div>
   );
 }
