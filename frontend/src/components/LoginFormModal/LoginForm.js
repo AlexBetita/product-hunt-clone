cd9fe6d30ba611d40acddb63b54f6b27514e1c95
AlexBetita <alexbheb25@gmail.com>
2021-05-25 18:22:48 -0700

    login modal done

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -22,32 +22,50 @@ function LoginForm() {
 
   return (
     <div className='div__login__modal__container'>
-      <form onSubmit={handleSubmit}>
-        <ul>
-          {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
-          ))}
-        </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
-      </form>
+      <div className='div__styles__login'>
+        <div className='div__login__image'>
+          <img className='image__login'
+            src='https://ph-static.imgix.net/category-tech/kitty.png?auto=format&auto=compress&codec=mozjpeg&cs=strip&w=100&h=92&fit=max&dpr=1'>
+          </img>
+        </div>
+        <h1 className='h1__login__sign'>
+          Log in Product Hunt Clone
+        </h1>
+        <p className='p__login__sign'>
+        Join our community of friendly folks discovering and sharing the latest products in the world.
+        </p>
+        <div className='div__form__styles'>
+          <form onSubmit={handleSubmit}>
+            <ul>
+              {errors.map((error, idx) => (
+                <li key={idx}>{error}</li>
+              ))}
+            </ul>
+            <label className='label__login'>
+              Username or Email
+              <input
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </label>
+            <label className='label__password'>
+              Password
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </label>
+            <button className='button__form__login' type="submit">Log In</button>
+          </form>
+        </div>
+        <p className='p__login__sign__2'>
+         Meow'll never post to any of your accounts without your permission.
+        </p>
+      </div>
     </div>
   );
 }
