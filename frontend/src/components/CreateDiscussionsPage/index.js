f375b61a85b168c42f1cc327326f88609eefcc95
AlexBetita <alexbheb25@gmail.com>
2021-06-29 03:04:35 -0700

    added more styling, and users can now post discussions

--- frontend/src/components/CreateDiscussionsPage/index.js
+++ frontend/src/components/CreateDiscussionsPage/index.js
@@ -1,10 +1,52 @@
 import { NavLink } from 'react-router-dom';
-
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { postDiscussion } from '../../store/discussions';
 
 import './CreateDiscussionsPage.css'
 
 
 const CreateDiscussionsPage = () => {
+
+    const dispatch = useDispatch()
+
+    const [discussion, setDiscussion] = useState('');
+    const [message, setMessage] = useState('');
+    const [errors, setErrors] = useState([]);
+
+    const handleSubmit = async (e) =>{
+        e.preventDefault();
+        let newErrors = [];
+
+        if(discussion.length < 4){
+            newErrors.push('Title is too short minimum 4 chars')
+        } else if (discussion.length > 100){
+            newErrors.push('Dicussion is too long maximum 100 chars')
+        }
+
+        if(message.length < 5){
+            newErrors.push('Message is too short minimum 5 chars')
+        } else if (message.length > 2500){
+            newErrors.push('Message is too long maximum 2500 chars')
+        }
+
+        if (!newErrors.length){
+            setErrors([]);
+            await dispatch(postDiscussion({ discussion, message }))
+                .catch(async (res) =>{
+                    const data = await res.json();
+                    if (data && data.errors){
+                        newErrors = data.errors;
+                        setErrors(newErrors);
+                    }
+                })
+        }
+
+        setErrors(newErrors)
+
+        return setErrors
+    }
+
     return (
         <>
             <div className='div__create__discussions__page__styles'>
@@ -26,9 +68,14 @@ const CreateDiscussionsPage = () => {
                             </li>
                         </ul>
                     </div>
+                    <ul>
+                        {errors.map((error, idx) => <li className='errors__create__discussions' key={idx}>{error}</li>)}
+                    </ul>
                     <div className='create__discussions__styles'>
                         <div>
-                            <form className='create__discussions__form'>
+                            <form
+                                onSubmit={handleSubmit}
+                                className='create__discussions__form'>
                                 <label className='create__discussions__title__style'>
                                     <div className='div__create__discussions__title__style'>
                                         <span className='create__discussions__title'>
@@ -36,8 +83,11 @@ const CreateDiscussionsPage = () => {
                                         </span>
                                     </div>
                                     <input
+                                    type='text'
                                     className='input__create__discussions__title'
                                     placeholder='Try to describe what this discussion will be about'
+                                    value={discussion}
+                                    onChange={(e)=>setDiscussion(e.target.value)}
                                     >
                                     </input>
                                 </label>
@@ -50,9 +100,12 @@ const CreateDiscussionsPage = () => {
                                             </div>
                                             <div>
                                                 <textarea
+                                                type="textarea"
                                                 placeholder='Write your message here or create a poll (Top right icon)'
-                                                className='text__area__create__discussions'>
-
+                                                className='text__area__create__discussions'
+                                                value={message}
+                                                onChange={(e)=>setMessage(e.target.value)}
+                                                >
                                                 </textarea>
                                             </div>
                                         </label>
